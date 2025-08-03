358e35753d01de20570df1081fe3d07671f22655
shah-krish <krishshah1411@gmail.com>
2025-08-03 07:46:40 -0400

    add addProject function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+const projects = []; //will use this array to store all the projects
+
+function addProject(){
+    let name = prompt("Please enter the name of your project: ");
+    if(name!=null){
+        projects.push(name);
+    }
+    console.log("projects = "+projects);
+}
+
+function addTask(){
+    if(projects.length==0){
+        prompt("Please add a project first!");
+    }
+    
+}
