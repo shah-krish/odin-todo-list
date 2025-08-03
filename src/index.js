95cd5f80a334ae85bedf02bd2810ac5125e07687
shah-krish <krishshah1411@gmail.com>
2025-08-03 08:31:40 -0400

    Finish addTask, saveTask function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 const projects = []; //will use this array to store all the projects
+let tasks = new Map(); //will use this map/dict to link projects and tasks
 
 function addProject(){
     let name = prompt("Please enter the name of your project: ");
@@ -11,7 +12,40 @@ function addProject(){
 
 function addTask(){
     if(projects.length==0){
-        prompt("Please add a project first!");
+        alert("Please add a project first!");
+        return;
     }
-    
+    const form = document.createElement("form");
+    const projectOptions = projects.map(project => `<option value="${project}">${project}</option>`).join("");
+    form.id = "taskForm";
+    form.innerHTML = `
+    <select id="projectSelect" required>
+      <option value="" disabled selected>Select a project</option>
+      ${projectOptions}
+    </select><br>
+    <input type="text" placeholder="Task" id="task" required><br>
+    <button type="submit">Add Task</button>
+    <button type="button" id="cancelBtn">Cancel</button>
+  `;
+    form.addEventListener("submit", saveTask);
+    const cancelBtn = form.querySelector("#cancelBtn");
+    cancelBtn.addEventListener("click", () => form.remove());
+    document.body.appendChild(form);
 }
+function saveTask(){
+    const taskName = document.querySelector("#task").value;
+    const taskProject = document.querySelector("#projectSelect").value;
+    if (!tasks.has(taskProject)) {
+        tasks.set(taskProject, []);
+      }
+    tasks.get(taskProject).push(taskName);
+    document.getElementById("taskForm").remove();
+/*     console.log(`Added task "${taskName}" to project "${taskProject}"`);
+    console.log(tasks); */
+}
+
+const addProj = document.querySelector(".newProject");
+addProj.addEventListener("click", addProject);
+const addT = document.querySelector(".newList");
+addT.addEventListener("click", addTask);
+
